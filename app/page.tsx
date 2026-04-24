bbd744c36082d690544f614c66d0db6d870b3116
limakinji-dot <limakinji@gmail.com>
2026-04-24 18:08:48 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
 
   return (
     <>
-      <CustomCursor />
       {!loaded && <Preloader onComplete={handlePreloaderComplete} />}
 
       <main ref={containerRef} className="relative bg-[#030303]">
